fcea7f852111be986c0eb3fea159caa53aee00bc
Julio Espinosa <julio.i.espinosa@gmail.com>
2020-04-01 00:03:31 -0500

    Trying to add some args brehh... it's confusing.

--- v2/py_comm.py
+++ v2/py_comm.py
@@ -1,5 +1,56 @@
+import argparse
 from py_comm_server import py_comm_server as pcs
 
 
+def run_this_shit():
+    pass
+
+
 if __name__ == "__main__":
-    x = pcs()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', action='store',
+                        dest='simple_value',
+                        help='Store a simple value')
+
+    parser.add_argument('-c', action='store_const',
+                        dest='constant_value',
+                        const='value-to-store',
+                        help='Store a constant value')
+
+    parser.add_argument('-t', action='store_true',
+                        default=False,
+                        dest='boolean_t',
+                        help='Set a switch to true')
+
+    parser.add_argument('-f', action='store_false',
+                        default=True,
+                        dest='boolean_f',
+                        help='Set a switch to false')
+
+    parser.add_argument('-a', action='append',
+                        dest='collection',
+                        default=[],
+                        help='Add repeated values to a list')
+
+    parser.add_argument('-A', action='append_const',
+                        dest='const_collection',
+                        const='value-1-to-append',
+                        default=[],
+                        help='Add different values to list')
+
+    parser.add_argument('-B', action='append_const',
+                        dest='const_collection',
+                        const='value-2-to-append',
+                        help='Add different values to list')
+
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s 1.0')
+
+    results = parser.parse_args()
+
+    print('simple_value     = {!r}'.format(results.simple_value))
+
+    #print('This is what I input as arg:', results.simple_value)
+
+    run_this_shit()
